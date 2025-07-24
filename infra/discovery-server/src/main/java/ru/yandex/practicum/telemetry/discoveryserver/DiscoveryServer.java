5a4d33b1b70e884c3eda0b1198e36173c9170e2d
Alecksey Kiselev <Kiselev.Alecksey@yandex.ru>
2025-07-24 11:16:44 +0300

    add Spring Cloud Discovery Eureka

--- infra/discovery-server/src/main/java/ru/yandex/practicum/telemetry/discoveryserver/DiscoveryServer.java
+++ infra/discovery-server/src/main/java/ru/yandex/practicum/telemetry/discoveryserver/DiscoveryServer.java
@@ -1,9 +1,13 @@
+package ru.yandex.practicum.telemetry.discoveryserver;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@ConfigurationPropertiesScan
 public class DiscoveryServer {
     public static void main(String[] args) {
         SpringApplication.run(DiscoveryServer.class, args);
